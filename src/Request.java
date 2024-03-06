fca6f256a1dbf3becaa0f885f4cc1824aa4aa15d
Darpyface <alex.dirk.tempel@gmail.com>
2024-03-06 12:08:19 -0500

    Added methods to be implemented for Request

--- src/Request.java
+++ src/Request.java
@@ -1,10 +1,15 @@
+import java.net.DatagramPacket;
+
 public class Request {
     private final int startingFloor;
     private final int destinationFloor;
     private boolean finished;
 
-    Request(int startingFloor, int destinationFloor) {
+    Request(int startingFloor, int destinationFloor) throws IllegalArgumentException {
         finished = false;
+        if (startingFloor == destinationFloor) {
+            throw new IllegalArgumentException("starting floor can't be equal to destination floor");
+        }
         this.startingFloor = startingFloor;
         this.destinationFloor = destinationFloor;
     }
@@ -21,5 +26,33 @@ public class Request {
         this.finished = true;
     }
 
-    //Will need a method to convert to string for UDP packets
+    public boolean isFinished() {
+        return finished;
+    }
+
+    /**
+     * Converts this Request into a string that can be put into a UDP packet
+     * @return String that can be put into a UDP packet
+     */
+    public String convertToPacketMessage() {
+        return ""; //Temporary body
+    }
+
+    /**
+     * Returns a Request object from a UDP packet
+     * @param packet the packet to be parsed into a Request
+     * @return Request according to the data in the packet
+     */
+    public static Request parsePacket(DatagramPacket packet) {
+        return new Request(1, 2); //Temporary body
+    }
+
+    /**
+     * Returns a Request object from a string, following the proper formatting
+     * @param message the string to be parsed into a Request
+     * @return Request according to the data in the string
+     */
+    public static Request parseString(String message) {
+        return new Request(1, 2); //Temporary body
+    }
 }
