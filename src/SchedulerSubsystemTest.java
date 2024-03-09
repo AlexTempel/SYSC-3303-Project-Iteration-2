b013111363e9c96c94737527ccc6f565cea34f2d
PeterWillis3 <peterwillis@cmail.carleton.ca>
2024-03-09 13:54:27 -0500

    added test branch for SchedulerSubsystem

---
+++ src/SchedulerSubsystemTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SchedulerSubsystemTest {
+
+    @org.junit.jupiter.api.Test
+    void getRequestFromInternet() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dealWithNewRequest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sendRequestToElevator() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkPending() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void selectElevator() {
+    }
+}
