526ab4db1d8528635e2c4aca7e6b7dc59499ae10
Jake <jakecalder@cmail.carleton.ca>
2024-03-09 14:48:46 -0500

    added test class for floor subsystem

---
+++ src/FloorSubsystemTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class FloorSubsystemTest {
+
+    @org.junit.jupiter.api.Test
+    void readCSV() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCurrentRequest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sendToScheduler() {
+    }
+}
