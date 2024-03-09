74a10473305af059552599cec841a34d76dc7863
Jake <jakecalder@cmail.carleton.ca>
2024-03-09 16:51:32 -0500

    editing readCSV test and added test input file

--- src/FloorSubsystemTest.java
+++ src/FloorSubsystemTest.java
@@ -1,16 +1,37 @@
-import static org.junit.jupiter.api.Assertions.*;
+import java.net.InetAddress;
+import java.util.ArrayList;
+
 
 class FloorSubsystemTest {
 
     @org.junit.jupiter.api.Test
-    void readCSV() {
+    void readCSV(String file) {
+        try{
+            InetAddress ipAddress = InetAddress.getLocalHost();
+            FloorSubsystem testFloor = new FloorSubsystem(16000, 17000, 5, ipAddress);
+            ArrayList<TimedRequest> testRequests = new ArrayList<TimedRequest>();
+            String file = "TestInput.txt";
+            testRequests = readCSV();
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
     @org.junit.jupiter.api.Test
     void getCurrentRequest() {
+        try{
+
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
     @org.junit.jupiter.api.Test
     void sendToScheduler() {
+        try{
+
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 }
