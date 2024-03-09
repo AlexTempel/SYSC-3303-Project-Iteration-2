2feddf83c1616ad2c488536d222079881abefcba
PeterWillis3 <peterwillis@cmail.carleton.ca>
2024-03-09 14:12:24 -0500

    Added test for selectElevator

--- src/SchedulerSubsystemTest.java
+++ src/SchedulerSubsystemTest.java
@@ -1,4 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.net.SocketException;
 
 class SchedulerSubsystemTest {
 
@@ -18,7 +21,19 @@ class SchedulerSubsystemTest {
     void checkPending() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void selectElevator() {
+        try {
+            Request req = new Request(1,4,10);
+            SchedulerSubsystem testScheduler = new SchedulerSubsystem(1);
+
+            testScheduler.selectElevator(req);
+
+            assertTrue(testScheduler.get(0).getInUse);
+
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
